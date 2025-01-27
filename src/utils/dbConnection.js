71405b49a90cdaeff8bb0acfc2b103d8f4b910c4
norayna <rayna@bozhkova.info>
2025-01-27 18:36:15 +0000

    Remove commented out code

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -1,16 +1,6 @@
 import {Firestore} from '@google-cloud/firestore'
-import {fetchProjectId, fetchServiceLevelCredentials} from './metadata.js'
-import {homedir} from 'os'
 
 export const getDbConnection = async () => {
-    const projectId = await fetchProjectId()
-    // const defaultServiceCredentials = await fetchServiceLevelCredentials()
-    //console.log(defaultServiceCredentials)
-    /*const db = new Firestore({
-        projectId,
-        keyFilename: homedir() + '/.config/gcloud/application_default_credentials.json',
-    })*/
     const db = new Firestore()
-    // const db = getFirestore(app);
     return db
 }
