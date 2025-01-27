722a52016bdeb9533436d156b5581a6a22d26ce9
norayna <rayna@bozhkova.info>
2025-01-27 12:57:25 +0000

    Add themes

--- src/news/themes/analyseThemes.js
+++ src/news/themes/analyseThemes.js
@@ -1,14 +1,38 @@
+/**
+ * Enum for theme roles
+ * @readonly
+ * @enum string
+ */
+const Role = Object.freeze({
+    PROBLEM: 'Problem',
+    JOY: 'Joy',
+    JUST_A_THING: 'Just a thing'
+});
+
 export const analyseThemes = async (news, languageClient) => {
     const document = {
         content: news,
         type: 'PLAIN_TEXT',
     };
-    const [entitySentiments] = await languageClient.analyzeEntitySentiment({document});
-    // const themes = entitySentiments.map(extractThemeFromEntitySentiment)
-    // return themes
-    return []
+    const [entitySentimentAnalysis] = await languageClient.analyzeEntitySentiment({document});
+    const entities = entitySentimentAnalysis.entities
+    const themes = entities.map(extractThemeFromEntity)
+    return themes
 }
 
-const extractThemeFromEntitySentiment = (entitySentiment) => {
+const extractThemeFromEntity = (entity) => {
+    let role = Role.JUST_A_THING
 
+    if (entity.sentiment.score > 0) {
+        role = Role.JOY
+    } else if (entity.sentiment.score < 0) {
+        role = Role.PROBLEM
+    }
+    
+    const theme = {
+        name: entity.name,
+        role
+    }
+
+    return theme
 }
