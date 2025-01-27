1b4cc5727f30ad04232a900360b1eef05b205409
norayna <rayna@bozhkova.info>
2025-01-27 12:04:56 +0000

    Tidy up db connection file

--- src/utils/dbConnection.js
+++ src/utils/dbConnection.js
@@ -3,14 +3,10 @@ import {fetchProjectId} from './metadata.js'
 import {homedir} from 'os'
 
 export const getDbConnection = async () => {
-    try {
-        const projectId = await fetchProjectId()
-        const db = new Firestore({
-            projectId,
-            keyFilename: homedir() + '/.config/gcloud/application_default_credentials.json',
-        })
-        return db
-    } catch (e) {
-        console.error(e)
-    }
+    const projectId = await fetchProjectId()
+    const db = new Firestore({
+        projectId,
+        keyFilename: homedir() + '/.config/gcloud/application_default_credentials.json',
+    })
+    return db
 }
