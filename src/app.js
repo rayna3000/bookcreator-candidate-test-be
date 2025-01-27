ca27002685bb6a91b4634e1663b0790ff058abfc
norayna <rayna@bozhkova.info>
2025-01-27 18:40:56 +0000

    Re-enable opaque exception handler

--- src/app.js
+++ src/app.js
@@ -68,9 +68,6 @@ app.get('/themes', validateGetThemesFilter, async (req, res, next) => {
   }
 })
 
-// app.use(handleExceptions)
-app.use((err, req, res, next) => {
-  res.send(err)
-})
+app.use(handleExceptions)
 
 export default app
